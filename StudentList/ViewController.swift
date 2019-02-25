3c6128146762dfd327edd8f509424ca2b6fecb1d
Alice Liang <alicel_@alices-macbook-air.local>
2019-02-25 16:39:43 -0500

    Initial Commit

---
+++ StudentList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudentList
+//
+//  Created by Alice Liang on 2/25/19.
+//  Copyright © 2019 Alice Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
